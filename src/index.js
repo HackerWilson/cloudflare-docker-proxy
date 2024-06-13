67be36b1ae2cbcb459bf9f42e5b7958663b630b5
Wilson <wenbinwb1@foxmail.com>
2024-06-13 16:36:31 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -4,13 +4,13 @@ addEventListener("fetch", (event) => {
 });
 
 const routes = {
-  "docker.libcuda.so": "https://registry-1.docker.io",
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.hustbot.com": "https://registry-1.docker.io",
+  "quay.hustbot.com": "https://quay.io",
+  "gcr.hustbot.com": "https://gcr.io",
+  "k8s-gcr.hustbot.com": "https://k8s.gcr.io",
+  "k8s.hustbot.com": "https://registry.k8s.io",
+  "ghcr.hustbot.com": "https://ghcr.io",
+  //"cloudsmith.hustbot.com": "https://docker.cloudsmith.io",
 };
 
 function routeByHosts(host) {
